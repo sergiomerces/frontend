220fd130a34895e7815efe2b3e016eed6f621288
sergiomerces <sergio.merces@gmail.com>
2023-09-24 23:36:01 -0300

    função de alta ordem forEach()

--- javascript/aula11/foreach.js
+++ javascript/aula11/foreach.js
@@ -1,11 +1,29 @@
+/*
+função de alta ordem de array
+
+array.forEach(index, item, arrayCompleto);
+*/
+
 const linguagens = ['HTML', 'CSS', 'JavaScript', 'Nodejs', 'PHP', 'SQL'];
 console.clear();
 
+/*foreach com retorno dos valores*/
+console.log('Retorno dos valores');
 linguagens.forEach(item => {
     console.log(item);
 });
-console.log('\n\n');
 
+console.log('\n');
+
+/*foreach com retorno dos índices*/
+console.log('Retorno dos valores e índices');
 linguagens.forEach((item, index) => {
     console.log(index, item);
+});
+
+console.log('\n');
+
+/*foreach com retorno dos índices, valores e array completo*/
+linguagens.forEach((item, index, arrayCompleto) => {
+    console.log(index, item, arrayCompleto);
 });
