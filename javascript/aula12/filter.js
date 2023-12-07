a6fa899b70803273fd065e3c9becf71fd40db654
sergiomerces <sergio.merces@gmail.com>
2023-12-07 14:56:23 -0300

    correções de comentários

--- javascript/aula12/filter.js
+++ javascript/aula12/filter.js
@@ -1,12 +1,12 @@
 /*
-método que chama a função passada como parâmetro para cada elemento do array e devolve um novo array com vaores que atendem a condição estabelecida
+método que chama a função passada como parâmetro para cada elemento do array e devolve um novo array com valores que atendem a condição estabelecida
 */
 console.clear();
 
 const numero = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-console.table(numero)
-console.log('Pesquisa')
+console.table(numero);
+console.log('Pesquisa');
 const pesquisa = numero.filter(item => (item % 2 === 0));
 
 console.log(pesquisa);
