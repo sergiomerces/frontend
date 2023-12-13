d4190426154b851a1d60252174d481c9d75e1eea
sergiomerces <sergio.merces@gmail.com>
2023-12-13 01:43:38 -0300

    adicionando estilos com JavaScript

--- front-dinamico/aula21/js/script.js
+++ front-dinamico/aula21/js/script.js
@@ -1,6 +1,7 @@
 const botaoDiminuir = document.querySelector('button#btn-diminuir');
 const botaoAumentar = document.querySelector('button#btn-aumentar');
 const contador = document.querySelector('h2#contador');
+const input = document.querySelector('#input');
 
 botaoDiminuir.addEventListener('click', (event) => {
     const valorAtual = Number(contador.textContent);
@@ -11,3 +12,14 @@ botaoAumentar.addEventListener('click', (event) => {
     const valorAtual = Number(contador.textContent);
     contador.textContent = valorAtual + 1;
 });
+
+input.addEventListener('input', (event) => {
+    console.log(input.value);
+});
+
+//adicionando estilos inline ao contador
+contador.style.color = 'red';
+contador.style.padding = '3rem';
+contador.style.backgroundColor = 'silver';
+contador.style.border = '1px solid black';
+contador.style.width = '650px';
